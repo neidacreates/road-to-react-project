9831f4beac189bfef73345d256fa0ae36fef649a
neidacreates <neidaxmoreno@gmail.com>
2022-06-08 15:42:49 -0400

    added a list to main component

--- src/App.js
+++ src/App.js
@@ -5,17 +5,49 @@ import * as React from 'react';
 //the App component is just a JS function (called a function component)
 // it doesn't receice any parameters right now, but it can (props)
 
-function getTitle(title){
-  return title;
-}
+// array defined outside of the component
+const list = [
+  {
+    title: 'React',
+    url: 'https"//reactjs.org/',
+    author: 'Jordan Walke',
+    num_components: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_components: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle("React")}</h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type ="text"/>
+
+      <hr />
       
+      <ul>
+        {list.map(function(item){
+          return ( 
+            <li key ={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_components}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
     // <div className="App">
     //   <header className="App-header">
