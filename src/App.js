8e4cb2530c0ef08f7b255aca4088107c8e567a11
neidacreates <neidaxmoreno@gmail.com>
2022-06-17 19:50:23 -0400

    added props to generate list

--- src/App.js
+++ src/App.js
@@ -6,26 +6,30 @@ import * as React from 'react';
 // it doesn't receice any parameters right now, but it can (props)
 
 // array defined outside of the component
-const list = [
-  {
-    title: 'React',
-    url: 'https"//reactjs.org/',
-    author: 'Jordan Walke',
-    num_components: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_components: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
 
-const App = () => (
+
+const App = () => {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https"//reactjs.org/',
+      author: 'Jordan Walke',
+      num_components: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_components: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  return (
     <div>
       <h1>My Hacker Stories</h1>
       
@@ -33,49 +37,47 @@ const App = () => (
 
       <hr />
 
-      <List />
+      <List list={stories} />
     </div>
   );
+};
 
-const List = () => (
+const List = (props) => (
     <ul>
-      {list.map(function (item) {
+      {props.list.map(function (item) {
         return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_components}</span>
-            <span>{item.points}</span>
-          </li>
+          <Item key={item.objectID} item={item} />
         );
     })}
     </ul>
   );
 
-const Search = () => (
+const Search = () => {
+
+  // function for the change event of the input field (event handler)
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type ="text"/>
+      <input id="search" type ="text" onChange={handleChange}/>
     </div>
   );
 
-export default App;
+};
 
- // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
+
+
+export default App;
